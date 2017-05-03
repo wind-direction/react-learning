277f85e5c982b88901f4782911b69147bdd2dd5f
wind_direction <wind.direction.work@gmail.com>
2017-05-03 10:18:49 +0000

    mofidy chapter 8

--- ruanyf-es6tutorial/src/chapter8.js
+++ ruanyf-es6tutorial/src/chapter8.js
@@ -94,7 +94,84 @@ describe('1. 函数参数的默认值', function(){
     let foo1 = bar();
     expect(foo1).to.equal('outer');
   });
+});
 
-  it('(7) ')
+describe('2. reset参数', function(){
+  it('(1) function func(...vals){} vals指向函数的多余参数，是一个数组，不再使用arguments. func(1,2,3)',function(){
+    function func(...vals){
+      expect(vals[0]).to.equal(1);
+      expect(vals[1]).to.equal(2);
+      expect(vals[2]).to.equal(3);
+    }
+
+    func(1,2,3);
+  });
+
+  it('(2) 判断arguments与reset参数一致,func(1,2,3)', function(){
+    function func(){
+      return Array.prototype.slice.call(arguments).sort();
+    }
+
+    function func1(...values){
+      return values;
+    }
+
+    let a = func(1,2,3);
+    let b = func1(1,2,3);
+    a.forEach(function(item, key){
+      expect(item).to.equal(b[key]);
+    });
+  });
+});
+
+describe('3. 扩展运算符', function(){
+  it('(1) 扩展运算符主要用于函数调用f(-1, ...[0,1], 2, ...[3])=>[-1,0,1,2,3]', function(){
+    function f(v,w,x,y,z){
+      return [v,w,x,y,z];
+    }
+
+    let args = [0,1];
+    let b = f(-1, ...args,2, ...[3]);
+    expect(b[0]).to.equal(-1);
+    expect(b[1]).to.equal(0);
+    expect(b[2]).to.equal(1);
+    expect(b[3]).to.equal(2);
+    expect(b[4]).to.equal(3);
+  });
+  it('(2)替代数组的apply方法, Math.max.apply(null, [14,3,77]) = Math.max(...[14,3,77]) = 77', function(){
+    let maxA = Math.max.apply(null, [14,3,77]),
+        maxB = Math.max(...[14,3,77]);
+    expect(maxA).to.equal(maxB);
+  });
+  it('(3-1) 扩展运算符的应用：合并数组： [1,2].concat([3,4]) === [...[1,2], ...[3,4]]', function(){
+    let arr1 = [1,2].concat([3,4]),
+        arr2 = [...[1,2], ...[3,4]];
+    arr1.forEach(function(item, key){
+      expect(item).to.equal(arr2[key]);
+    });
+  });
+  it('(3-2) 扩展运算符的应用：与解构赋值结合： [first, ...rest] = [1,2,3,4,5]=>first = 1, rest=[2,3,4,5]', function(){
+    let [first, ...rest] = [1,2,3,4,5];
+    expect(first).to.equal(1);
+    expect(rest.length).to.equal(4);
+    rest.forEach(function(item, key){
+      expect(item).to.equal(key + 2);
+    });
+  });
+});
+
+describe('6. 箭头函数', function() {
+  it('(1) this指针的变化', function(){
+    let foo = () => {
+      let a = () => {
+        console.log('id:', this.id);
+      };
+      a();
+    };
+
+    let id = 3;
+
+    foo({id: 2});
+  });
 });
 
