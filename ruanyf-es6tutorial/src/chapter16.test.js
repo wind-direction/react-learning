7da31a388e36e7e80bc5ff0d81d9d897be3357c1
wind_direction <wind.direction.work@gmail.com>
2017-05-25 10:56:20 +0000

    增加部分笔记+ 16章的前三小节

--- ruanyf-es6tutorial/src/chapter16.test.js
+++ ruanyf-es6tutorial/src/chapter16.test.js
@@ -55,12 +55,154 @@ describe('1. 简介', () => {
     expect(res).to.deep.equal([1,2,3,4,5,6]);
   });
 
-  it('(4) ')
+  it('(4) yield表达式如果用在另一个表达式之中，必须放在圆括号里面。', () => {
+    function * demo() {
+      console.log('Hello' + (yield) );
+    }
+
+    let g = demo();
+    g.next();
+  });
+
+  it('(5) 由于 Generator 函数就是遍历器生成函数，因此可以把 Generator 赋值给对象的Symbol.iterator属性，从而使得该对象具有 Iterator 接口。', () => {
+    let myIterable = {};
+    myIterable[Symbol.iterator] = function *() {
+      yield 1;
+      yield 2;
+      yield 3;
+    };
+
+    expect([...myIterable]).to.deep.equal([1,2,3]);
+  });
+
+  it('(6) Generator 函数执行后，返回一个遍历器对象。该对象本身也具有Symbol.iterator属性，执行后返回自身。', () => {
+    function* gen() {}
+
+    let g = gen();
+
+    expect(g[Symbol.iterator]()).to.deep.equal(g);
+  })
 });
 
-describe('2. next 方法的参数', () => {});
+describe('2. next 方法的参数', () => {
+  it('(1) yield表达式本身没有返回值，或者说总是返回undefined。next方法可以带一个参数，该参数就会被当作上一个yield表达式的返回值。', () => {
+    function* f() {
+      for(let i = 0; true; i++) {
+        let reset = yield i;
+        if(reset) { i = -1;}
+      }
+    }
 
-describe('3. for...of 循环', () => {});
+    let g = f();
+    expect(g.next()).to.deep.equal({value: 0, done: false});
+    expect(g.next()).to.deep.equal({value: 1, done: false});
+    expect(g.next()).to.deep.equal({value: 2, done: false});
+    expect(g.next(true)).to.deep.equal({value: 0, done: false});
+    expect(g.next()).to.deep.equal({value: 1, done: false});
+  });
+
+  it('(2) 可以在 Generator 函数运行的不同阶段，从外部向内部注入不同的值，从而调整函数行为。', () => {
+    function* foo(x) {
+      let y = 2 *(yield (x + 1));
+      let z = yield (y / 3);
+      return (x + y + z);
+    }
+
+    let a = foo(5);
+    expect(a.next()).to.deep.equal({value: 6, done: false});
+    // a.next() : 等同于 yield (x + 1) 变成了 undefined；那么y 就成了 2*undefined = NaN， 之后执行yield(NaN/3) 输出NaN
+    expect(a.next()).to.deep.equal({value: NaN, done: false});
+    // a.next() : 等同于 yield (y / 3) 变成了undefined；那么z 就成了 undefined ， return (5 + NaN + undefined) = NaN
+    expect(a.next()).to.deep.equal({value: NaN, done: true});
+
+    let b = foo(5);
+    expect(b.next()).to.deep.equal({value: 6, done: false});
+    // b.next(12) : 等同于 yield (x + 1) 变成了12；那么y 就成了24， 之后执行yield(24/3) 输出8
+    expect(b.next(12)).to.deep.equal({value: 8, done: false});
+    // b.next(13) : 等同于 yield (y / 3) 变成了13；那么z 就成了13， return (5 + 24 + 13) = 42;
+    expect(b.next(13)).to.deep.equal({value: 42, done: true});
+  });
+
+  it('(3) 如果想要第一次调用next方法时，就能够输入值，可以在 Generator 函数外面再包一层。', () => {
+    function wrapper(generatorFunction) {
+      return function( ...args) {
+        let generatorObject = generatorFunction(...args);
+        generatorObject.next();
+        return generatorObject;
+      };
+    }
+
+    const wrapped = wrapper(function* () {
+      console.log(`First input: ${yield}`);
+      return 'DONE';
+    });
+
+    wrapped().next('hello!');
+  });
+
+  it('(4) 再看一个通过next方法的参数，向 Generator 函数内部输入值的例子。', () => {
+    function* dataConsumer() {
+      console.log('Started');
+      console.log(`1. ${yield}`);
+      console.log(`2. ${yield}`);
+      return 'result';
+    }
+
+    let genObj = dataConsumer();
+    genObj.next();
+    genObj.next('a');
+    genObj.next('b');
+  });
+});
+
+describe('3. for...of 循环', () => {
+  it('(1) 再看一个通过next方法的参数，向 Generator 函数内部输入值的例子。', () => {
+    function* fibonacci() {
+      let [prev, curr] = [0, 1];
+      for(;;) {
+        [prev ,curr] = [curr, prev + curr];
+        yield curr;
+      }
+    }
+
+    let res = [];
+    for(let n of fibonacci()) {
+      if(n > 100) break;
+      res.push(n);
+    }
+    expect(res).to.deep.equal([ 1, 2, 3, 5, 8, 13, 21, 34, 55, 89 ]);
+  });
+
+  it('(2) 原生的 JavaScript 对象没有遍历接口，无法使用for...of循环，通过 Generator 函数为它加上这个接口，就可以用了。', () => {
+    function* objectEntries(obj) {
+      let propKeys = Reflect.ownKeys(obj);
+
+      for(let propKey of propKeys) {
+        yield [propKey, obj[propKey]];
+      }
+    }
+
+    let jane = {first:'Jane', last: 'Doe'};
+    for(let [key, value] of objectEntries(jane)){
+      console.log(`${key}: ${value}`);
+    }
+  });
+
+  it('(3) 加上遍历器接口的另一种写法是，将 Generator 函数加到对象的Symbol.iterator属性上面。', () => {
+    function* objectEntries() {
+      let propKyes = Object.keys(this);
+      for(let propKey of propKyes) {
+        yield [propKey, this[propKey]];
+      }
+    }
+
+    let jane = {first: 'Jane', last: 'Doe'};
+    jane[Symbol.iterator] = objectEntries;
+    for(let [key, value] of jane) {
+      console.log(`${key}: ${value}`);
+    }
+  });
+});
 
 describe('4. Generator.prototype.throw()', () => {});
 
